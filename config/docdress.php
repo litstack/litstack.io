13e6858ea77fc0b02ca9d0a6baaecd5d2d045e0b
cbl <lennart.carbe@gmail.com>
2020-08-25 09:38:21 +0200

    init laravel

---
+++ config/docdress.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'path' => resource_path('docs'),
+
+    'repos' => [
+        'my/repo' => [
+            'route_prefix'    => 'docs',
+            'subfolder'       => null,
+            'default_page'    => 'introduction',
+            'default_version' => 'master',
+            'versions'        => [
+                'master' => 'Master',
+            ],
+        ],
+    ],
+
+    'themes' => [
+        'default' => [
+            'primary' => '#4951f2',
+
+            'code-bg'            => '#f5f8fb',
+            'code-selection'     => '#b3d4fc',
+            'code-value'         => '#055472',
+            'code-prop'          => '#d44545',
+            'code-function'      => '#4951f2',
+            'code-variable'      => '#588bbd',
+            'code-string'        => '#169f0c',
+            'code-default-color' => '#090910',
+        ],
+    ],
+];
