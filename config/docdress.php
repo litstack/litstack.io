f6ee1792f9eb2a5840319e2c3a4f079b298a9127
cbl <lennart.carbe@gmail.com>
2020-08-25 20:42:18 +0200

    fix

--- config/docdress.php
+++ config/docdress.php
@@ -6,21 +6,22 @@ return [
     'open_external_links_in_new_tab' => true,
 
     'repos' => [
-        'my/repo' => [
+        'litstack/docs' => [
             'route_prefix'    => 'docs',
             'subfolder'       => null,
-            'default_page'    => 'introduction',
+            'default_page'    => 'installation',
             'default_version' => 'master',
+            'webhook_token'   => env('WEBHOOK_TOKEN', null),
             'versions'        => [
                 'master' => 'Master',
+                '2.4'    => '2.4',
             ],
         ],
     ],
 
     'themes' => [
         'default' => [
-            'primary' => '#4951f2',
-
+            'primary'            => '#4951f2',
             'code-bg'            => '#f5f8fb',
             'code-selection'     => '#b3d4fc',
             'code-value'         => '#055472',
