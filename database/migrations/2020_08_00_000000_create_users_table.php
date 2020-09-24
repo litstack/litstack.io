572ee09360997543cc7a2aac2c478e4bc862280c
cbl <lennart.carbe@gmail.com>
2020-09-24 16:17:37 +0200

    fix

--- database/migrations/2020_08_00_000000_create_users_table.php
+++ database/migrations/2020_08_00_000000_create_users_table.php
@@ -13,7 +13,6 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::dropIfExists('users');
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
