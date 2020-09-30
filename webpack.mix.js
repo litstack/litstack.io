5331339710ef3f095ece578d06e7c2a0161a2aff
cbl <lennart.carbe@gmail.com>
2020-09-30 18:22:55 +0200

    wip

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,11 +2,6 @@ const mix = require("laravel-mix");
 const tailwindcss = require("tailwindcss");
 
 mix.js("resources/js/app.js", "public/js")
-    .js("vendor/aw-studio/docdress/assets/js/search.js", "public/docdress/js")
-    .sass(
-        "vendor/aw-studio/docdress/assets/sass/app.scss",
-        "public/docdress/css"
-    )
     .sass("resources/sass/app.scss", "public/css")
     .options({
         processCssUrls: false,
