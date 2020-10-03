229e39b281e98427903394ef5784e4593202f139
cbl <lennart.carbe@gmail.com>
2020-10-03 14:57:00 +0200

    demo btn

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -10,8 +10,8 @@
                     Set up appealing Laravel admin panels in record time and make your administrative tasks enjoyable.
                 </h2>
                 <div>
-                    <a href="{{ url('docs') }}" class="mb-2 mr-4 btn btn-primary">
-                        Read the docs
+                    <a href="https://demo.litstack.io/admin" class="mb-2 mr-4 btn btn-primary">
+                        Try the demo
                     </a>
                     <a href="https://github.com/litstack/litstack" class="mb-2 btn btn-outline">
                         <svg viewBox="0 0 21 20" class="absolute w-6 h-6 -ml-4 fill-current " xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path id="a" d="M0 0h20.48v19.627H0z"/></defs><g fill="none" fill-rule="evenodd"><mask id="b" fill="#fff"><use xlink:href="#a"/></mask><path d="M10.24 0C4.582 0 0 4.506 0 10.063c0 4.446 2.934 8.217 7.002 9.547.512.094.7-.217.7-.484 0-.24-.01-.872-.013-1.711-2.849.607-3.45-1.35-3.45-1.35-.466-1.162-1.139-1.472-1.139-1.472-.927-.624.072-.611.072-.611 1.028.07 1.568 1.037 1.568 1.037.913 1.538 2.397 1.094 2.983.837.092-.651.356-1.094.648-1.346-2.274-.252-4.664-1.117-4.664-4.972 0-1.099.397-1.996 1.054-2.7-.115-.255-.461-1.278.09-2.663 0 0 .857-.27 2.815 1.031a9.996 9.996 0 012.56-.34c.87.006 1.741.117 2.56.34 1.946-1.301 2.804-1.031 2.804-1.031.55 1.385.204 2.408.102 2.663a3.86 3.86 0 011.05 2.7c0 3.865-2.394 4.716-4.672 4.964.358.302.69.919.69 1.862 0 1.346-.012 2.428-.012 2.755 0 .264.18.579.704.478 4.097-1.321 7.028-5.095 7.028-9.534C20.48 4.506 15.895 0 10.24 0" fill="#070A1F" mask="url(#b)"/></g></svg>
